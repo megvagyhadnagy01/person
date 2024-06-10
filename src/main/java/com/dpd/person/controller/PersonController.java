19110a4ec01c5ff54faea0034e9ec02d757efca5
megvagyhadnagy01 <megvagyhadnagy01@gmail.com>
2024-06-10 21:19:26 +0200

    -Added PersonController

---
+++ src/main/java/com/dpd/person/controller/PersonController.java
@@ -0,0 +1,39 @@
+package com.dpd.person.controller;
+import com.dpd.person.model.Person;
+import com.dpd.person.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/persons")
+public class PersonController {
+
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping
+    public List<Person> getAllPersons() {
+        return personService.getAllPersons();
+    }
+
+    @PostMapping
+    public Person createPerson(@RequestBody Person person) {
+        return personService.createPerson(person);
+    }
+
+    @PutMapping("/{id}")
+    public Person updatePerson(@PathVariable Long id, @RequestBody Person personDetails) {
+        return personService.updatePerson(id, personDetails);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePerson(@PathVariable Long id) {
+        personService.deletePerson(id);
+    }
+
+    @PutMapping("/depersonalize/{id}")
+    public Person depersonalizePerson(@PathVariable Long id) {
+        return personService.depersonalizePerson(id);
+    }
+}
