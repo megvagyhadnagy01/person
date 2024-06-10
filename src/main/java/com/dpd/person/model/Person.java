1644067843237f4a54551b42ab4142adcc13cc91
megvagyhadnagy01 <megvagyhadnagy01@gmail.com>
2024-06-10 20:32:10 +0200

    -Added model Person.java

---
+++ src/main/java/com/dpd/person/model/Person.java
@@ -0,0 +1,88 @@
+package com.dpd.person.model;
+import jakarta.persistence.*;
+
+//import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "persons")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Date birthDate;
+    private String birthPlace;
+    private String motherMaidenName;
+    private String socialSecurityNumber;
+    private String taxId;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public String getMotherMaidenName() {
+        return motherMaidenName;
+    }
+
+    public void setMotherMaidenName(String motherMaidenName) {
+        this.motherMaidenName = motherMaidenName;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getTaxId() {
+        return taxId;
+    }
+
+    public void setTaxId(String taxId) {
+        this.taxId = taxId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+}
