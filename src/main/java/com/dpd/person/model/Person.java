d1defb73f0feeb2ad106b750d81fc660755cedf8
megvagyhadnagy01 <megvagyhadnagy01@gmail.com>
2024-06-10 21:04:25 +0200

    -Modifyed person.java and use Lombok

--- src/main/java/com/dpd/person/model/Person.java
+++ src/main/java/com/dpd/person/model/Person.java
@@ -1,11 +1,16 @@
 package com.dpd.person.model;
 import jakarta.persistence.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 //import javax.persistence.*;
 import java.util.Date;
 
 @Entity
 @Table(name = "persons")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,70 +23,6 @@ public class Person {
     private String socialSecurityNumber;
     private String taxId;
     private String email;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getBirthPlace() {
-        return birthPlace;
-    }
-
-    public void setBirthPlace(String birthPlace) {
-        this.birthPlace = birthPlace;
-    }
-
-    public String getMotherMaidenName() {
-        return motherMaidenName;
-    }
-
-    public void setMotherMaidenName(String motherMaidenName) {
-        this.motherMaidenName = motherMaidenName;
-    }
-
-    public String getSocialSecurityNumber() {
-        return socialSecurityNumber;
-    }
-
-    public void setSocialSecurityNumber(String socialSecurityNumber) {
-        this.socialSecurityNumber = socialSecurityNumber;
-    }
-
-    public String getTaxId() {
-        return taxId;
-    }
-
-    public void setTaxId(String taxId) {
-        this.taxId = taxId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
 
 
