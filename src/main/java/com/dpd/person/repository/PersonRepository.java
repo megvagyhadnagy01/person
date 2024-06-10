ada0db75d890e6b733509680ff4b634bdce5ebeb
megvagyhadnagy01 <megvagyhadnagy01@gmail.com>
2024-06-10 20:46:26 +0200

    -Added repository

---
+++ src/main/java/com/dpd/person/repository/PersonRepository.java
@@ -0,0 +1,8 @@
+package com.dpd.person.repository;
+import com.dpd.person.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
