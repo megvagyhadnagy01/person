ece2e3e5c27f3a1ff5b4ceb0da6b887cad434358
megvagyhadnagy01 <megvagyhadnagy01@gmail.com>
2024-06-11 20:53:04 +0200

    -Added App.js and update application.properties

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PersonForm from './components/PersonForm';
+import PersonList from './components/PersonList';
+
+function App() {
+    return (
+        <div className="App">
+            <PersonForm />
+            <PersonList />
+        </div>
+    );
+}
+
+export default App;
